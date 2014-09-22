9aff0151318f766fade12f71c28aabbc2fa79566
dan <dan.bate@gmail.com>
2014-09-22 23:44:48 +0100

    added sqlite database settings to dev settings

--- omegablog/blog_site/_developer_settings.py
+++ omegablog/blog_site/_developer_settings.py
@@ -1,2 +1,11 @@
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(os.path.dirname(__file__), "..", "db.sqlite3"),
+    }
+}
